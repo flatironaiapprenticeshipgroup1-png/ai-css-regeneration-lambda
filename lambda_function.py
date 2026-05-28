e85b90c8942a322976c509a81f1aee1504799661
rlhill888 <rlhill888@gmail.com>
2026-05-28 10:02:12 -0400

    removed debugging log

--- lambda_function.py
+++ lambda_function.py
@@ -81,8 +81,6 @@ def lambda_handler(event, context):
             secrets_client.get_secret_value(SecretId=os.environ["SECRET_NAME"])["SecretString"]
         )
         client = OpenAI(api_key=secret["OpenAIAPIKey"])
-        print("stuff:")
-        print(secret["OpenAIAPIKey"])
 
         for record in event["Records"]:
             body = json.loads(record["body"])
